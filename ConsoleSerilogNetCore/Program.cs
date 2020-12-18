541d0ef1213203afb0d15d5538a908d81022f31e
Ben Sampica <bsampica@Homesteaderslife.com>
2020-12-18 10:09:49 -0600

    Add project files.

---
+++ ConsoleSerilogNetCore/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace ConsoleSerilogNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = GetLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, "Something went wrong...");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+                ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .Build();
+
+            return configurationBuilder;
+        }
+
+        public static ILogger GetLogger()
+        {
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(GetConfiguration())
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
---
+++ ConsoleSerilogNetCore/README.txt
@@ -0,0 +1,4 @@
+﻿1. Install Serilog.AspNetCore.
+2. Install Serilog.Sinks.File.
+3. Replace the "Logging" JSON object with "Serilog" inside of appsettings.json.
+4. (Optional) Configure appsettings per environment.
