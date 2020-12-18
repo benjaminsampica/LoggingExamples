08674996cac7af9d4fa16dbabd4b3e8add8aade8
Ben Sampica <bsampica@Homesteaderslife.com>
2020-12-18 11:14:46 -0600

    Add Web app

---
+++ WebAppSerilogNetCore/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace WebAppSerilogNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SetStaticLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, "Something went wrong...");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseSerilog();
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+                ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .Build();
+
+            return configurationBuilder;
+        }
+
+        public static void SetStaticLogger()
+        {
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(GetConfiguration())
+                .CreateLogger();
+
+            Log.Logger = logger;
+        }
+    }
+}
